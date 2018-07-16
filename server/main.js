4804068ff806fcf66466af9a4b15244ce2b68e20
abhioxic <abhinandanshah96@gmail.com>
2018-07-16 11:44:21 +0530

    75/6 Basic URL redirection from _id.

--- server/main.js
+++ server/main.js
@@ -2,12 +2,25 @@ import { Meteor } from 'meteor/meteor';
 import { WebApp } from 'meteor/webapp';
 
 import '../imports/api/users';
-import '../imports/api/links';
+import {Links} from '../imports/api/links';
 import '../imports/startup/simpl-schema-configuration.js'
 
 Meteor.startup(() => {
 		WebApp.connectHandlers.use('/', (req, res, next) => {
 		console.log(req.url, req.method, req.headers, req.query);
+		const _id = req.url.slice(1);
+		const link = Links.findOne({ _id });
+
+		if( link ){
+			// Set Status Code
+			res.statusCode = 302;
+			// Set HTTP Headers
+			res.setHeader('Location', link.url);
+			// Set HTTP Body
+			// res.write('<h1>Hello</h1>');
+			// End response
+			res.end();
+		}
 		next();
 	});
 });
